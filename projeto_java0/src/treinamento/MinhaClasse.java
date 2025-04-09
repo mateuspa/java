1852cbf62bbbfb2556e4ca6a7405afb6fcd51c69
Mateus Prince <mateus.prince@gmail.com>
2025-04-08 22:50:30 -0300

    criando package

---
+++ projeto_java0/src/treinamento/MinhaClasse.java
@@ -0,0 +1,16 @@
+package treinamento;
+public class MinhaClasse {
+
+public static void main (String[] args) {
+    String nome = "Mateus";
+    String sobrenome = "Prince Antunes";
+
+    String nomeCompleto = nomeCompleto (nome, sobrenome);
+    System.out.println(nomeCompleto);
+}
+
+public static String nomeCompleto (String nome, String sobrenome){
+    return "Nome completo: "+nome+" "+sobrenome;
+}
+
+}
