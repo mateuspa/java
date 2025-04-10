925b958bf984fa57b44409bb75f42a0337af2b3a
Mateus Prince <mateus.prince@gmail.com>
2025-04-10 18:32:01 -0300

    utilizando Scanner para entradas

--- projeto_java0/src/AboutMe.java
+++ projeto_java0/src/AboutMe.java
@@ -1,10 +1,26 @@
+import java.util.Locale;
+import java.util.Scanner;
 public class AboutMe{
 
-    public static void main (String[] args){
+    public static void main (String[] args) throws Exception{
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        
+        /* Utilizando String args para receber os argumentos. Configurar String args no launch.json para usar no Terminal.
         String nome = args[0];
         String sobrenome = args[1];
         int idade = Integer.valueOf(args[2]);
         double altura = Double.valueOf(args[3]);
+        */
+
+        // Usando classe Scanner para ler entrada do console com padrão de separação sendo <tecla espaço>
+        System.out.println("Digite seu nome: ");
+        String nome = scanner.next();
+        System.out.println("Digite seu sobrenome: ");
+        String sobrenome = scanner.next();
+        System.out.println("Digite sua idade: ");
+        int idade = scanner.nextInt();
+        System.out.println("Digite sua altura em metros: ");
+        double altura = scanner.nextDouble();
 
         System.out.println("\nOla, me chamo: " + nome + " "+sobrenome);
         System.out.println("Tenho " + idade + " " + "anos");
