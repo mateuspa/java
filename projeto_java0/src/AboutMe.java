fb0c0a1699634190d648c3de629bd2370a73df8d
Mateus Prince <mateus.prince@gmail.com>
2025-04-11 19:18:09 -0300

    incluindo tratamento geral de exceções

--- projeto_java0/src/AboutMe.java
+++ projeto_java0/src/AboutMe.java
@@ -4,27 +4,34 @@ public class AboutMe{
 
     public static void main (String[] args) throws Exception{
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
-        
-        /* Utilizando String args para receber os argumentos. Configurar String args no launch.json para usar no Terminal.
-        String nome = args[0];
-        String sobrenome = args[1];
-        int idade = Integer.valueOf(args[2]);
-        double altura = Double.valueOf(args[3]);
-        */
+        try {
+            /* Utilizando String args para receber os argumentos. Configurar String args no launch.json para usar no Terminal.
+            String nome = args[0];
+            String sobrenome = args[1];
+            int idade = Integer.valueOf(args[2]);
+            double altura = Double.valueOf(args[3]);
+            */
 
-        // Usando classe Scanner para ler entrada do console com padrão de separação sendo <tecla espaço>
-        System.out.println("Digite seu nome: ");
-        String nome = scanner.next();
-        System.out.println("Digite seu sobrenome: ");
-        String sobrenome = scanner.next();
-        System.out.println("Digite sua idade: ");
-        int idade = scanner.nextInt();
-        System.out.println("Digite sua altura em metros: ");
-        double altura = scanner.nextDouble();
+            // Usando classe Scanner para ler entrada do console com padrão de separação sendo <tecla espaço>
+            System.out.println("Digite seu nome: ");
+            String nome = scanner.next();
+            System.out.println("Digite seu sobrenome: ");
+            String sobrenome = scanner.next();
+            System.out.println("Digite sua idade: ");
+            int idade = scanner.nextInt();
+            System.out.println("Digite sua altura em metros: ");
+            double altura = scanner.nextDouble();
 
-        System.out.println("\nOla, me chamo: " + nome + " "+sobrenome);
-        System.out.println("Tenho " + idade + " " + "anos");
-        System.out.println("Minha altura é " + altura + " m\n");
+            scanner.close();
+
+            System.out.println("\nOla, me chamo: " + nome + " "+sobrenome);
+            System.out.println("Tenho " + idade + " " + "anos");
+            System.out.println("Minha altura é " + altura + " m\n");
+        }
+
+        catch (Exception exception){
+            System.out.print("\n!!! O sistema deu um erro ainda nao tratado... !!!\n ");
+        }
     }
     
 }
