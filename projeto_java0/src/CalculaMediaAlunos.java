21ea206978f38415c7b3da2a6e9b1bbeddeaf6bc
Mateus Prince <mateus.prince@gmail.com>
2025-04-13 17:08:27 -0300

    For-Each e formatação printf

--- projeto_java0/src/CalculaMediaAlunos.java
+++ projeto_java0/src/CalculaMediaAlunos.java
@@ -5,28 +5,24 @@ public class CalculaMediaAlunos {
     public static void main(String[] args) throws Exception {
         try {
             Scanner scan = new Scanner(System.in);
-            String[] alunos = { "Mateus", "Camila", "Lucas", "Bruna", "Pedro" };
+            String[] alunos = { "Mateus", "Lucas", "Michel", "Humberto", "Carolina" };
 
             double media = calculaMediaTurma(alunos, scan);
 
             System.out.printf("Média da turma %.2f", media); // formata media com 2 casas decimais
         }
-
         catch (Exception InputMismatchException) {
             System.out.printf("Problema com o formato do número digitado!! Tente novamente...");
         }
-
     }
 
     public static double calculaMediaTurma(String[] alunos, Scanner scanner) {
-
         double soma = 0;
         for (String aluno : alunos) {
             System.out.printf("Nota do aluno %s: ", aluno);
             double nota = scanner.nextDouble();
             soma += nota;
         }
-
         return soma / alunos.length;
     }
 }
